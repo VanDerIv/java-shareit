69220605201f29c4bf159119e00d020fa025be41
ivandr <ivandr@bk.ru>
2023-05-24 08:54:29 +0300

    feat: тз15 style 7

---
+++ src/test/java/ru/practicum/shareit/ShareItAppTest.java
@@ -0,0 +1,15 @@
+package ru.practicum.shareit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.equalTo;
+
+class ShareItAppTest {
+    @Test
+    void main() {
+        ShareItApp.main(new String[] {});
+        assertThat(true, equalTo(true));
+    }
+}
